52d0a83ead39cedd83c8a5b48716d7c3816f29e0
Iswarya Tirumalasetti <aishwarya.tirumalasetty9@gmail.com>
2023-06-19 22:43:14 +0530

    second commit

---
+++ src/test/java/testcases/TestAssert.java
@@ -0,0 +1,30 @@
+package testcases;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestAssert {
+    WebDriver driver=null;
+    public void testAssert()
+    {
+
+        WebDriverManager.chromedriver().setup();
+        String ExpectedTitle=" Demo Web Shop. Login";
+        String chromeDriverPath = "C:\\software\\chromedriver_ver113\\chromedriver\\chromedriver.exe";
+        System.setProperty("webDriver.chrome.driver", chromeDriverPath);
+        driver=new ChromeDriver();
+        driver.get("https://demowebshop.tricentis.com/login");
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+        String ActualTitle=driver.getTitle();
+        System.out.println("the title of the application is:"+ActualTitle);
+        Assert.assertEquals(ActualTitle,ExpectedTitle);
+        driver.quit();
+    }
+
+
+}
